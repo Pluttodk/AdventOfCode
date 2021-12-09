a85c8361ce862f2a8f5972fd8265be2e9780f679
Mathias Oliver Valdbjørn Jørgensen - MOVJ <MOVJ@ATP.DK>
2021-12-09 08:44:44 +0100

    Day 1 in haskell

--- 2021/day1.hs
+++ 2021/day1.hs
@@ -1,8 +1,24 @@
 f :: [String] -> [Integer]
 f = map read
 
+par1 :: [Integer] -> Integer -> Integer
+par1 [x] acc = acc
+par1 (x:xs:xss) acc 
+    | xs > x = par1 (xs:xss) acc+1
+    | otherwise = par1 (xs:xss) acc
+
+par2 :: [Integer] -> Integer -> Integer
+par2 [x] acc = acc
+par2 (x:y:z:w:xss) acc 
+    | x+y+z < y+z+w = par2 (y:z:w:xss) acc+1
+    | otherwise = par2 (z:w:xss) acc
+
 main :: IO()
 main = do
     contents <- readFile "data/day1.txt"
-    let numbers = f contents
-    print numbers
+    let get_words = words contents
+    let numbers = f get_words
+    let part1 = par1 numbers 0
+    print part1
+    let part2 = par2 numbers 0
+    print part2
