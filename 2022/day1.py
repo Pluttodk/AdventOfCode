a788bf8c88c4441f2b19284231860c3dffa9f2ca
Mathias Oliver Valdbjørn Rønnelund - MOVJ <MOVJ@ATP.DK>
2022-12-01 08:03:29 +0100

    Day 1 done

--- 2022/day1.py
+++ 2022/day1.py
@@ -1,1 +1,16 @@
-print("PLACEHOLDER!")
+i = list(map(lambda x: x.strip(), open("1.in", "r").readlines()))
+
+elfs = [[]]
+for number in i:
+    if number != "":
+        elfs[-1].append(int(number))
+    else:
+        elfs.append([])
+
+elfs_total = list(map(sum,elfs))
+
+#1
+print(max(elfs_total))
+
+#2 
+print(sum(sorted(elfs_total, reverse=True)[:3]))
