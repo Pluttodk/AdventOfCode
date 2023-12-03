a1eda7f074473baf470e7972cdbecf59ce34134f
Mathias Oliver Valdbjørn Rønnelund - MOVJ <MOVJ@ATP.DK>
2023-12-03 21:42:31 +0100

    Day 3 done and dusted! And so am I!

--- 2023/day3.hs
+++ 2023/day3.hs
@@ -1,14 +1,9 @@
-{-# LANGUAGE OverloadedStrings #-}
-import qualified Data.Text as T
-import qualified Data.List as D
-import qualified Data.Text.IO as TIO
 import Data.Char (isDigit, digitToInt)
+import Data.List (group, sort, groupBy)
 
 board :: [[Char]]
 board = ["467..114..","...*......","..35..633.","......#...","617*......",".....+.58.","..592.....","......755.","...$.*....",".664.598.."]
 
-
--- Get all the digits from a single line, seperated by column, row (digit, column, row)
 getDigits :: [Char] -> [Char] -> Int -> [(Int, Int)]
 getDigits [] [] _ = []
 getDigits [] possibleDigit row = [(read possibleDigit, row - length possibleDigit)]
@@ -28,45 +23,61 @@ digitsOnBoard :: [[(Int, Int, Int)]]
 digitsOnBoard = addColToDigits rowDigit 0
     where rowDigit = findDigitsOnBoard board
 
+checkLocation :: [[Char]] -> (Int, Int) -> Bool
+checkLocation b (col, row)
+    | not (locVal == '.' || isDigit locVal) = True
+    | otherwise = False
+    where locVal = b !! col !! row
+
 checkDigit :: [[Char]] -> (Int, Int, Int) -> Int
 checkDigit b (val, col, row)
-    | b !! col !! row == '.' = val
-        where 
-            minCol = max [col-1, 0]
-            maxCol = min [col+1, length b]
-            minRow = max [row+length (show val), length (head b)]
-            maxRow = min [row-1, 0]
-
-checkAbove :: [[Char]] -> Int -> Int -> Bool
-checkAbove board col row
-    | row <= 0 = False
-    | otherwise = not (isDigitOrDot aboveField)
-    where
-        aboveField = board !! (col-1) !! row
-        isDigitOrDot c = c `elem` ['0'..'9'] || c == '.'
-
-isTextValid :: T.Text -> [[Char]] -> Int -> Int -> Int
-isTextValid value charBoard col row
-    | value == "" = 0
-    | any (\r -> checkAbove charBoard col r) rows = read (T.unpack value)
+    | any (checkLocation b) locations = val
     | otherwise = 0
+        where 
+            minCol = max (col-1) 0
+            maxCol = min (col+1) (length b - 1)
+            minRow = max (row-1) 0
+            maxRow = min (row+length (show val)) (length (head b) - 1)
+            locations = [(x,y) | x <- [minCol..maxCol], y <- [minRow..maxRow]]
+
+part1Sol :: [[Char]] -> Int
+part1Sol board = sum $ map (checkDigit board) (concat digitsOnBoard)
     where 
-        rows = [col .. T.length value + col]
+        rowDigit = findDigitsOnBoard board
+        digitsOnBoard = addColToDigits rowDigit 0
 
--- Returns -1 if no adjacent dots in the board is a symbol
-isLineValid :: [[T.Text]] -> [[Char]] -> Int -> Int -> Int
-isLineValid board charBoard col row
-    | checkAbove charBoard col row = read (T.unpack pos)
+part1 :: Int
+part1 = sum $ map (checkDigit board) (concat digitsOnBoard)
+
+
+-- Part 2
+
+findAllStarSymbols:: [[Char]] -> (Int, Int, Int) -> [(Int, Int, Int)]
+findAllStarSymbols b (val, col, row) = filter (\(x, y, _) -> (b !! x !! y) == '*') locations
     where 
-        pos = board !! col !! row
+        minCol = max (col-1) 0
+        maxCol = min (col+1) (length b - 1)
+        minRow = max (row-1) 0
+        maxRow = min (row+length (show val)) (length (head b) - 1)
+        locations = [(x,y,val) | x <- [minCol..maxCol], y <- [minRow..maxRow]]
 
+allStarLocations :: [[Char]] -> [(Int, Int, Int)]
+allStarLocations b = concat $ map (findAllStarSymbols b) (concat digitsOnBoard)
+    where 
+        rowDigit = findDigitsOnBoard b
+        digitsOnBoard = addColToDigits rowDigit 0
 
+removeDuplicates :: [(Int, Int, Int)] -> [[(Int, Int, Int)]]
+removeDuplicates tuples = filter (\g -> length g > 1) $ groupBy (\(a, b, _) (c, d, _) -> a == c && b == d) $ sort tuples
+
+part2Sol :: [[Char]] -> Int
+part2Sol board = sum $ map (\x -> product $ map (\(_,_,val) -> val) x) (removeDuplicates (allStarLocations board)) 
 
 main :: IO()
 main = do
-    contents <- TIO.readFile "3.in"
-    let symbolsBoard = T.lines contents
-    let board = D.map (T.splitOn ".") symbolsBoard
-    let charBoard = D.map T.unpack symbolsBoard
-    let value = isLineValid board charBoard 0 0
-    print value
+    contents <- readFile "3.in"
+    let vals = lines contents
+    let part1 = part1Sol vals
+    print (show part1)
+    let part2 = part2Sol vals
+    print (show part2)
