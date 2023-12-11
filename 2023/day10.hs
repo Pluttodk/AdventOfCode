47c54e9ccc361d990986a14a54d153b898534ff5
Mathias Oliver Valdbjørn Rønnelund - MOVJ <MOVJ@ATP.DK>
2023-12-11 08:42:08 +0100

    zero progress

--- 2023/day10.hs
+++ 2023/day10.hs
@@ -3,13 +3,6 @@ import qualified Data.Set as Set
 import Data.List (maximumBy)
 import Data.Ord (comparing)
 
-parseNodes :: [[Char]] -> Int -> Int -> [(Int, Int, Char)]
-parseNodes input col row 
-    | row < length x-1 = (col, row, x !! row):parseNodes input col (row+1)
-    | col < length input-1 = (col, row, x !! row):parseNodes input (col+1) 0
-    | otherwise = [(col, row, x !! row)]
-    where x = input !! col
-
 findStart :: [[Char]] -> Int -> Int -> (Int, Int, Int)
 findStart input col row 
     | row < length x && x !! row == 'S' = (col, row, 0)
@@ -22,7 +15,7 @@ startNeighbors :: [[Char]] -> (Int, Int, Int) -> [(Int, Int, Int)] -> [(Int, Int
 startNeighbors input (row, col, depth) acc
     | notElem (row+1, col, depth+1) acc && (row+1) < length input && input !! (row+1) !! col `elem` "|LJ" = 
         startNeighbors input (row, col, depth) ((row+1, col, depth+1):acc)
-    | notElem (row-1, col, depth+1) acc && (row-1) >= 0 && input !! (row-1) !! col `elem` "|7FLJ" = 
+    | notElem (row-1, col, depth+1) acc && (row-1) >= 0 && input !! (row-1) !! col `elem` "|7F" = 
         startNeighbors input (row, col, depth) ((row-1, col, depth+1):acc)
     | notElem (row, col-1, depth+1) acc && (col-1) >= 0 && input !! row !! (col-1) `elem` "-FL" = 
         startNeighbors input (row, col, depth) ((row, col-1, depth+1):acc)
@@ -59,27 +52,38 @@ parseToInt input (((from_row, from_col, from_depth),(at_row, at_col, at_depth)):
         (nRow, nCol) = mapDirection (from_row, from_col) (at_row, at_col) value
         stillToVisit = res ++ [((at_row, at_col, at_depth),(nRow, nCol, at_depth+1))]
 
+-- Not a coorect solution. Instead I should take a location that is not in the loop
+-- And do graph traversal from any non loop to the outside. If possible that value is not inside. Else Inside
 isInside :: [[Char]] -> (Int, Int) -> [(Int, Int)] -> Bool
 isInside input (row, col) loop
-    | checkPath rightPath && checkPath leftPath = True
+    | (row, col) `elem` loop = False
+    | checkRight > 0 && checkLeft > 0 && (checkRight `mod` 2 == 1 || checkLeft `mod` 2 == 1) = True
     | otherwise = False
     where 
         width = length input - 1
         width_inner = length (head input) - 1
         rightPath = [(row, x) | x <- [col .. width_inner]]
         leftPath = [(row, x) | x <- [0 .. col]]
-        checkPath path = length (filter (`elem` loop) path) `mod` 2 == 1
+        checkPath path = length (filter (`elem` loop) path)
+        checkRight = checkPath rightPath
+        checkLeft = checkPath leftPath
 
--- part2 :: [[Char]] -> (Int, Int) -> [(Int, Int)] -> Int -> Int
--- part2 input (row,col) loop score 
---     | notElem (row, col) loop && isInside input (row,col) loop = 
+part2 :: [[Char]] -> (Int, Int) -> [(Int, Int)] -> Int -> Int
+part2 input (row,col) loop score 
+    | notElem (row, col) loop && isInside input (row,col) loop && shouldGoRight = part2 input (row, col+1) loop (score+1)
+    | notElem (row, col) loop && isInside input (row,col) loop && shouldGoDown = part2 input (row+1, 0) loop (score+1)
+    | otherwise = score
+    where 
+        shouldGoRight = col < length (head input)-1
+        shouldGoDown = row < length input-1
 
 main :: IO()
 main = do
     -- Parsing
     contents <- readFile "10.in"
     let vals = lines contents
-    let start = findStart vals 0 0
+    let (start_row, start_col, start_depth) = findStart vals 0 0
+    let start = (start_row, start_col, start_depth)
     let neighbors = startNeighbors vals start []
     let directions = map (start, ) neighbors
     let loop = parseToInt vals directions []
@@ -88,6 +92,8 @@ main = do
     print (bestPart)
 
     -- part 2
-    let loopWithoutDepth = map (\(x,y,_) -> (x,y)) loop
-    print (isInside vals (4,3) loopWithoutDepth)
-    print "Done!"
+    let loopWithoutDepth = (start_row, start_col):map (\(x,y,_) -> (x,y)) loop
+    let location = concat [[(x,y) | y <- [0..length (head vals)-1]] | x <- [0..length vals-1]]
+    let insides = filter (\(col,row) -> isInside vals (col,row) loopWithoutDepth) location
+    print (insides)
+    print (length insides)
