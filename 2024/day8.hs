088f786d95744bc24978be2d7f2da06adb8322f5
Pluttodk <mat@valdbjorn.com>
2024-12-08 22:15:50 +0100

    adding comments

--- 2024/day8.hs
+++ 2024/day8.hs
@@ -4,11 +4,7 @@ import Data.List (nub)
 import Data.Text qualified as T
 import Data.Text.IO qualified as TIO
 
-data Point = Point {
-    x ::Int,
-    y :: Int
- } deriving (Show, Eq)
-
+-- General helper functions for finding coordinates
 findCoordinates :: Char -> [T.Text] -> [(Int, Int)]
 findCoordinates value textGrid =
     concatMap findInRow (zip [0..] textGrid)
@@ -16,15 +12,18 @@ findCoordinates value textGrid =
     findInRow (rowIndex, line) =
         [(rowIndex, colIndex) | (colIndex, char) <- zip [0..] (T.unpack line), char == value]
 
+-- Move coordinates to a new location
 moveCoordinates :: (Int, Int) -> (Int, Int) -> (Int, Int)
 moveCoordinates (fromX, fromY) (toX, toY) = (toX+distX, toY+distY)
     where
         distX = toX - fromX
         distY = toY - fromY
 
+-- Check the validity for location. Is used partially later on when parsing
 isValidPoint :: Int -> Int -> (Int, Int) -> Bool
 isValidPoint width height (x,y) = x>=0 && x < width && y>=0 && y < height
 
+-- Part 1
 findAllPoints :: ((Int, Int) -> Bool) -> [(Int, Int)] -> [(Int, Int)]
 findAllPoints checkValid (a:b:rs)
     | aValid && bValid = aMove:bMove:rest
@@ -57,15 +56,18 @@ findAllPointsP2 _ _ = []
 
 main :: IO ()
 main = do
+    -- Load data
   contents <- TIO.readFile "8.in"
   let lines = map T.unpack (T.lines contents)
   let characters = ['0'..'9'] ++ ['a'..'z'] ++ ['A'..'Z']
   let coordinates = map (\x -> findCoordinates x (T.lines contents)) characters
   let checkValid = isValidPoint (length lines) (length (head lines))
+  -- Part 1
   let p1 = nub $ concatMap (findAllPoints checkValid) coordinates
   print coordinates
   print $ length p1
   print "Part 1"
+  -- Part 2
   let p2 = nub (concatMap (findAllPointsP2 checkValid) coordinates)
   print $ length p2
   print "part 2"
